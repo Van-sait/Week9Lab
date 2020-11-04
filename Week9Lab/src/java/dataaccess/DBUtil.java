048f3e4dfa8141d50d13f8ac24b540e2afb24d10
798382 <798382@ELF8IT221517.ACDM.DS.SAIT.CA>
2020-11-04 15:40:36 -0700

    Cloned Lab 8

--- Week9Lab/src/java/dataaccess/DBUtil.java
+++ Week9Lab/src/java/dataaccess/DBUtil.java
@@ -1,13 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dataaccess;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import java.sql.*;
 
 public class DBUtil {
-        private static final EntityManagerFactory emf =
-           Persistence.createEntityManagerFactory("UserPU");
-        
-        public static EntityManagerFactory getEmFactory() {
-            return emf;
+
+    public static void closePreparedStatement(Statement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
         }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
--- Week9Lab/src/java/dataaccess/RoleDB.java
+++ Week9Lab/src/java/dataaccess/RoleDB.java
@@ -5,33 +5,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.EntityManager;
 import models.Role;
 
 public class RoleDB {
 
     public List<Role> getAll() throws Exception {
-        EntityManager em = DBUtil.getEmFactory().createEntityManager();
-      
-        try {
-            List<Role> roles = em.createNamedQuery("Role.findAll", Role.class).getResultList();
-            return roles;
-            
-        } finally {
-            em.close();
-        }
-
-    }
-
-    public Role get(int id){
-        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        List<Role> roles = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT * FROM role";
         
         try {
-            Role role = em.find(Role.class, id);
-            return role;
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int roleId = rs.getInt(1);
+                String roleName = rs.getString(2);
+                Role role = new Role(roleId, roleName);
+                roles.add(role);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
         }
-        finally {
-            em.close();
-        }
+
+        return roles;
     }
+
 }
